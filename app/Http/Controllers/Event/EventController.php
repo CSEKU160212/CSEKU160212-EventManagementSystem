d6c4c7e413efcfee95ce4ce7923a45d7612d1f6f
CSEKU160212 <lotif1612@cseku.ac.bd>
2020-08-19 23:32:49 +0600

    Funtion added to EventController

--- app/Http/Controllers/Event/EventController.php
+++ app/Http/Controllers/Event/EventController.php
@@ -38,7 +38,38 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-       //
+       $request->validate([
+            'title' => 'required|string',
+            'organized_by' =>'required|string',
+            'venue' => 'required|string',
+            'event_start_date' => 'required|date',
+            'event_length' => 'required|string',
+            'reg_start_date' => 'required|date',
+            'reg_end_date' => 'required|date',
+            'decription' => 'require|string',
+            'contact_no' => 'require|string',
+            'contact_email' => 'require|string|email',
+       ]);
+
+        $event = new Event([
+            'title' => $request->title,
+            'organized_by' =>$request->organized_by,
+            'venue' => $request->venue,
+            'event_start_date' => $request->event_start_date,
+            'event_length' => $request->event_length,
+            'reg_start_date' => $request->reg_start_date,
+            'reg_end_date' => $request->reg_end_date,
+            'decription' => $request->decription,
+            'contact_no' => $request->contact_no,
+            'contact_email' => $request->contact_email,
+        ]);
+
+       if($event->save()){
+            return response()->json(['message' => 'Event created succesfully', 'event' => $event], 200 );
+       }else{
+            return response()->json(['message' => 'Failed to create event'], 400);
+       }
+
     }
 
     /**
@@ -47,9 +78,14 @@ class EventController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
-        //
+        $event = Event::find($id);
+        if($event){
+            return response()->json(['message' => 'Successfully found event', 'event' => $event], 200);
+        }else{
+            return response()->json(['message' => 'Failed to find event'], 400);
+        }
     }
 
     /**
